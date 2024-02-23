6daeeb20bd75b719fcb2608a2cbf1dccbe9d5561
EduardoSQ <eduardoooo89@hotmail.com>
2024-02-23 12:58:16 -0600

    Finish Output area

--- mainwindow.py
+++ mainwindow.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import time
+import threading
 
 class MainWindow():
     def __init__(self):
@@ -8,6 +10,7 @@ class MainWindow():
         self.__window.minsize(570, 463)
 
         self.__globalTimer = 0
+        self.__timerActive = True
         self.__idAfter = ''
 
 
@@ -24,6 +27,39 @@ class MainWindow():
         self.__generateProcessesButton.pack(side=tk.LEFT, padx=10)
         self.__globalTimerLabel.place(relx=1.0, x=-65, anchor=tk.NE)
 
+        #Output area
+        self.__outputArea = tk.Frame(self.__window, background="#dddddd")
+        self.__outputArea.pack()
 
+        #Running Process Output area
+        self.__processesWaitingOArea = tk.Frame(self.__outputArea, background="#dddddd")
+        self.__processesWaitingOArea.pack(side=tk.LEFT)
+
+        self.__processesWaitingLabel = tk.Label(self.__processesWaitingOArea, text="EN ESPERA", background="#dddddd")
+        self.__processesWaitingOutput = tk.Label(self.__processesWaitingOArea, width=22, height=20)
+        self.__missingBatchesLabel = tk.Label(self.__processesWaitingOArea, text="# de Lotes pendientes", background="#dddddd")
+        self.__processesWaitingLabel.pack()
+        self.__processesWaitingOutput.pack()
+        self.__missingBatchesLabel.pack(pady=10)
+
+        #Running Process Output area
+        self.__runningProcessOArea = tk.Frame(self.__outputArea, background="#dddddd")
+        self.__runningProcessOArea.pack(side=tk.LEFT, padx=20)
+
+        self.__runningProcessLabel = tk.Label(self.__runningProcessOArea, text="EN EJECUCIÓN", background="#dddddd")
+        self.__runningProcessOutput = tk.Label(self.__runningProcessOArea, width=22, height=10)
+        self.__runningProcessLabel.pack()
+        self.__runningProcessOutput.pack()
+
+        #Finished Processes Output area
+        self.__finishedProcessesOArea = tk.Frame(self.__outputArea, background="#dddddd")
+        self.__finishedProcessesOArea.pack(side=tk.LEFT)
+        
+        self.__finishedProcessesLabel = tk.Label(self.__finishedProcessesOArea, text="TERMINADOS", background="#dddddd")
+        self.__finishedProcessesOutput = tk.Label(self.__finishedProcessesOArea, width=22, height=20)
+        self.__getResultsButton = tk.Button(self.__finishedProcessesOArea, text="Obtener resultados")
+        self.__finishedProcessesLabel.pack()
+        self.__finishedProcessesOutput.pack()
+        self.__getResultsButton.pack(pady=10)
 
         self.__window.mainloop()
