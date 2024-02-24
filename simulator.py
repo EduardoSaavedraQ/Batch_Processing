f361171880a87de2bcf74e7ebe60d27ea78ee9d9
EduardoSQ <eduardoooo89@hotmail.com>
2024-02-24 13:51:47 -0600

    Add a callback option when there is an update on a process

--- simulator.py
+++ simulator.py
@@ -5,31 +5,47 @@ class Simulator():
         self.__solutions = []
         self.__ready = True
         self.__tbc = tbc
+        self.__currentBatch = 0
+        self.__currentProcess = 0
     
     def setTBC(self, tbc=1):
         self.__tbc = tbc
 
-    def simulateProcesses(self, batchesList:list):
+    def simulateProcesses(self, batchesList:list, action=None, args=None):
         self.__solutions = []
         self.__ready = False
+        self.__currentBatch = 0
+
         for batch in batchesList:
             solutionsSubList = []
+            self.__currentProcess = 0
+
             for process in batch:
                 while process["EMT"] > 0:
-                    print(process)
+
+                    if action is not None:
+                        if args is not None:
+                            action(*args)
+                        else:
+                            action()
+
                     process["EMT"] -= 1
                     time.sleep(self.__tbc)
+
                     if process["EMT"] == 0:
                         solution = {
                             "ProcessNumber": process["ProcessNumber"],
                             "Name": process["Name"],
                             "Operation": f"{process['FirstOperand']} {process['Operand']} {process['SecondOperand']} = {Simulator.__getOperation(process)}"
                         }
-                        solutionsSubList.append(solution)
-            self.__solutions.append(solutionsSubList)
-        
-        self.__ready = True
 
+                        solutionsSubList.append(solution)
+                        self.__currentProcess += 1
+
+            self.__solutions.append(solutionsSubList)
+            self.__currentBatch += 1
+
+        self.__ready = True
 
     def __getOperation(process:dict):
         if process["Operand"] == '+':
@@ -46,19 +62,28 @@ class Simulator():
 
     def getStatus(self):
         return self.__ready
-    
+
     def getSolutions(self):
         return self.__solutions
+
+    def getCurrentProcessStatus(self, batchesList:list):
+        return batchesList[self.__currentBatch][self.__currentProcess]
+
+def printCurrentProcessStatus(simulator:Simulator, batchesList:list):
+    print(simulator.getCurrentProcessStatus(batchesList))
+
+def test():
+    print("HOLA")
 
 if __name__ == '__main__':
     from processesGenerator import ProcessesGenerator
 
-    batches = ProcessesGenerator.generateRandomProcesses(3, 5)
+    batches = ProcessesGenerator.generateRandomProcesses(8, 5)
     ProcessesGenerator.to_txt("datos", batches)
 
     simulator = Simulator()
 
-    simulator.simulateProcesses(batches)
+    simulator.simulateProcesses(batches, printCurrentProcessStatus, (simulator, batches))
 
     solutions = simulator.getSolutions()
 
