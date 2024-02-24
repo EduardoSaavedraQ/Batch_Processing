845a1e0d622433e2de8fe3792fdb4faba3a228c0
EduardoSQ <eduardoooo89@hotmail.com>
2024-02-23 18:03:25 -0600

    Remove testing methods

--- main.py
+++ main.py
@@ -2,20 +2,5 @@
 from mainwindow import MainWindow
 import tkinter as tk
 
-def main():
-    app = MainWindow()
-    return 0
-
 if __name__ == '__main__':
-    #main()
-    """ root = tk.Tk()
-    root.configure(width=200, height=200, background="black")
-
-    f1 = tk.Frame(width=50, height=50, background="red")
-    f2 = tk.Frame(width=50, height=50, background="green")
-
-    f1.pack(side="right")
-    f2.place(x=100, y=100, anchor="n")
-
-    root.mainloop() """
     app = MainWindow()
