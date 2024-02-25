485baf66ca233dde1d457386da7557e669740b97
EduardoSQ <eduardoooo89@hotmail.com>
2024-02-25 02:07:59 -0600

    Add events listeners to simulator

--- simulator.py
+++ simulator.py
@@ -2,22 +2,56 @@ import time
 
 class Simulator():
     def __init__(self, tbc=1):
+        self.__batches = []
         self.__solutions = []
-        self.__ready = True
+        self.__currentBatchIndex = 0
+        self.__currentProcessSubindex = 0
+        self.__active = False
         self.__tbc = tbc
+        self.__actionsAfterStartSimulator = []
+        self.__actionsAfterUpdateEMT = []
+        self.__actionsAfterAppendSolution = []
+
+    def setBatches(self, batches:list):
+        self.__batches = batches
+    
+    def addEventListener(self, event:str, action):
+        if event == "onStartSimulator":
+            self.__actionsAfterStartSimulator.append(action)
+        elif event == "onUpdateEMT":
+            self.__actionsAfterUpdateEMT.append(action)
+        elif event == "onAppendSolution":
+            self.__actionsAfterAppendSolution.append(action)
+    
+    def __executeActionsAfterStartSimulator(self):
+        for action in self.__actionsAfterStartSimulator:
+            action()
+    
+    def __executeActionsAfterUpdateEMT(self):
+        for action in self.__actionsAfterUpdateEMT:
+            action()
+    
+    def __executeActionsAfterAppendSolution(self):
+        for action in self.__actionsAfterAppendSolution:
+            action()
     
     def setTBC(self, tbc=1):
         self.__tbc = tbc
 
-    def simulateProcesses(self, batchesList:list):
+    def simulateProcesses(self):
         self.__solutions = []
-        self.__ready = False
-        for batch in batchesList:
-            solutionsSubList = []
+        i = 0
+        for batch in batches:
+            self.__solutions.append([])
+            i += 1
+        self.__active = True
+        self.__executeActionsAfterStartSimulator()
+        for batch in self.__batches:
             for process in batch:
                 while process["EMT"] > 0:
-                    print(process)
+                    #print(process)
                     process["EMT"] -= 1
+                    self.__executeActionsAfterUpdateEMT()
                     time.sleep(self.__tbc)
                     if process["EMT"] == 0:
                         solution = {
@@ -25,10 +59,15 @@ class Simulator():
                             "Name": process["Name"],
                             "Operation": f"{process['FirstOperand']} {process['Operand']} {process['SecondOperand']} = {Simulator.__getOperation(process)}"
                         }
-                        solutionsSubList.append(solution)
-            self.__solutions.append(solutionsSubList)
+                        self.__solutions[self.__currentBatchIndex].append(solution)
+                        self.__executeActionsAfterAppendSolution()
+                        self.__currentProcessSubindex += 1
+                        #solutionsSubList.append(solution)
+            self.__currentBatchIndex += 1
+            self.__currentProcessSubindex = 0
+            #self.__solutions.append(solutionsSubList)
         
-        self.__ready = True
+        self.__active = True
 
 
     def __getOperation(process:dict):
@@ -44,28 +83,41 @@ class Simulator():
             else:
                 return process["FirstOperand"] / process["SecondOperand"]
 
-    def getStatus(self):
-        return self.__ready
+    def getSimulatorStatus(self):
+        return self.__active
     
     def getSolutions(self):
         return self.__solutions
+    
+    def getCurrentProcess(self):
+        return self.__batches[self.__currentBatchIndex][self.__currentProcessSubindex]
 
-if __name__ == '__main__':
-    from processesGenerator import ProcessesGenerator
+def greeting():
+    print("Hola")
 
-    batches = ProcessesGenerator.generateRandomProcesses(3, 5)
-    ProcessesGenerator.to_txt("datos", batches)
+def printCurrentProcessStatus(simulator:Simulator):
+    print(simulator.getCurrentProcess())
 
-    simulator = Simulator()
-
-    simulator.simulateProcesses(batches)
-
-    solutions = simulator.getSolutions()
-
+def printSolutions(simulator:Simulator):
     i = 1
-    for batch in solutions:
+    for batch in simulator.getSolutions():
         print("Lote" + str(i) + "\n----------------------------")
         for solution in batch:
             print(solution)
         print("------------------------------------\n")
         i += 1
+
+if __name__ == '__main__':
+    from processesGenerator import ProcessesGenerator
+
+    batches = ProcessesGenerator.generateRandomProcesses(8, 5)
+    ProcessesGenerator.to_txt("datos", batches)
+
+    simulator = Simulator()
+    simulator.addEventListener(event="onStartSimulator", action=greeting)
+    simulator.addEventListener(event="onUpdateEMT", action=lambda: printCurrentProcessStatus(simulator))
+    simulator.addEventListener(event="onAppendSolution", action=lambda: printSolutions(simulator))
+    simulator.addEventListener(event="onAppendSolution", action=greeting)
+
+    simulator.setBatches(batches)
+    simulator.simulateProcesses()
